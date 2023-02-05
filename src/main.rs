afe5ae6b444b7200e015c5808857bc9572f7443f
Nikita Volobuev <nikitavbv@gmail.com>
2023-02-05 21:52:04 +0200

    feat(filters): add simple filter

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,16 @@
 
 use {
     std::sync::Arc,
+    hyper::Method,
     crate::{
         server::run_http_server,
         utils::init_logging,
         runner::runner,
+        filters::HttpMethodFilter,
     },
 };
 
+pub mod filters;
 pub mod models;
 pub mod runner;
 pub mod server;
@@ -17,6 +20,6 @@ pub mod utils;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     init_logging();
-    run_http_server(Arc::new(runner)).await;
+    run_http_server(Arc::new(HttpMethodFilter::new(Method::POST, runner))).await;
     Ok(())
 }
