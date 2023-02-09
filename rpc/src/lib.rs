aa5a5313141edcb2f01715a9a96e899cb7e454d8
Nikita Volobuev <nikitavbv@gmail.com>
2023-02-09 23:24:14 +0200

    refactor(server): use tonic

--- rpc/src/lib.rs
+++ rpc/src/lib.rs
@@ -1,3 +1,3 @@
-tonic::include_proto!("lab");
+tonic::include_proto!("ml_sandbox");
 pub const FILE_DESCRIPTOR_SET: &[u8] =
-    tonic::include_file_descriptor_set!("lab_descriptor");
+    tonic::include_file_descriptor_set!("ml_sandbox_descriptor");
--- server/src/main.rs
+++ server/src/main.rs
@@ -2,30 +2,37 @@
 
 use {
     std::sync::Arc,
-    actix_web::{HttpServer, App},
-    crate::{
-        utils::init_logging,
-        app::AiLabApp,
+    tonic::{transport::Server, Status, Request, Response},
+    rpc::{
+        ml_sandbox_service_server::{MlSandboxService, MlSandboxServiceServer},
+        RunSimpleModelRequest,
+        RunSimpleModelResponse,
     },
+    crate::utils::init_logging,
 };
 
-pub mod app;
 pub mod models;
-pub mod runner;
 pub mod utils;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     init_logging();
     
-    let app = Arc::new(AiLabApp::new());
-
-    HttpServer::new(move || {
-        App::new()
-            .configure(|v| app.init(v))
-    })
-        .bind(("0.0.0.0", 8080))
-        .unwrap()
-        .run()
+    Server::builder()
+        .accept_http1(true)
+        .add_service(tonic_web::enable(MlSandboxServiceServer::new(MlSandboxServiceHandler)))
+        .serve("0.0.0.0:8080".parse().unwrap())
         .await
+        .unwrap();
+
+    Ok(())
+}
+
+struct MlSandboxServiceHandler;
+
+#[tonic::async_trait]
+impl MlSandboxService for MlSandboxServiceHandler {
+    async fn run_simple_model(&self, req: Request<RunSimpleModelRequest>) -> Result<Response<RunSimpleModelResponse>, Status> {
+        Ok(Response::new(RunSimpleModelResponse {}))
+    }
 }
