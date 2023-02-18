11b8992556915674be6d5d9d2d93906aa03c006f
Nikita Volobuev <nikitavbv@gmail.com>
2023-02-19 01:15:00 +0200

    feat(linux-gpu-env): download pytorch

--- linux-gpu-env.sh
+++ linux-gpu-env.sh
@@ -1,2 +1,4 @@
+wget https://download.pytorch.org/libtorch/cu116/libtorch-cxx11-abi-shared-with-deps-1.13.0%2Bcu116.zip
+unzip libtorch-cxx11-abi-shared-with-deps-1.13.0+cu116.zip
 export LIBTORCH=$(pwd)/libtorch
 export LD_LIBRARY_PATH=${LIBTORCH}/lib:$LD_LIBRARY_PATH
