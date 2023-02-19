d6c5344ffaa0292b5842eeb03773ba28faf84117
Nikita Volobuev <nikitavbv@gmail.com>
2023-02-19 14:22:29 +0200

    feat(linux-gpu-env): download torch only if not exists

--- linux-gpu-env.sh
+++ linux-gpu-env.sh
@@ -1,4 +1,8 @@
-wget https://download.pytorch.org/libtorch/cu116/libtorch-cxx11-abi-shared-with-deps-1.13.0%2Bcu116.zip
-unzip libtorch-cxx11-abi-shared-with-deps-1.13.0+cu116.zip
+if [ ! -d "./libtorch" ]
+then
+    wget https://download.pytorch.org/libtorch/cu116/libtorch-cxx11-abi-shared-with-deps-1.13.0%2Bcu116.zip
+    unzip libtorch-cxx11-abi-shared-with-deps-1.13.0+cu116.zip
+fi
+
 export LIBTORCH=$(pwd)/libtorch
 export LD_LIBRARY_PATH=${LIBTORCH}/lib:$LD_LIBRARY_PATH
