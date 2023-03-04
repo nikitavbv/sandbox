d2169c8b4ca196f3bd8cbac16df686997ad488be
Nikita Volobuev <nikitavbv@gmail.com>
2023-03-04 21:26:08 +0200

    feat(terraform): add frontend instance and dns

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -4,13 +4,22 @@ terraform {
       source = "vultr/vultr"
       version = "2.12.1"
     }
+    
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 3.0"
+    }
   }
 }
 
 provider vultr {
-  api_key = "${file(".secrets/vultr_api_key")}"
+  api_key = file(".secrets/vultr_api_key")
   rate_limit = 100
   retry_limit = 3
+}
+
+provider cloudflare {
+  api_token = file(".secrets/cloudflare_api_token")
 }
 
 variable tags {
@@ -29,7 +38,7 @@ resource vultr_object_storage object_storage {
   label = "${file(".secrets/object_storage_cluster_name")}"
 }
 
-// cloud instance to server frontend files
+// VPCs
 data vultr_region waw {
   filter {
     name = "id"
@@ -37,6 +46,17 @@ data vultr_region waw {
   }
 }
 
+resource vultr_vpc frontend {
+  region = data.vultr_region.waw.id
+  description = "sandbox-frontend"
+}
+
+resource vultr_vpc backend {
+  region = data.vultr_region.waw.id
+  description = "sandbox-backend"
+}
+
+// cloud instance to server frontend files
 data vultr_plan small_cpu_instance {
   filter {
     name = "locations"
@@ -64,4 +84,24 @@ resource vultr_instance frontend {
   tags = var.tags
   hostname = "sandbox-frontend"
   enable_ipv6 = true
+  vpc_ids = [vultr_vpc.frontend.id]
+  user_data = <<SCRIPT
+#!/usr/bin/env bash
+echo "does it work?" > /root/test
+SCRIPT
+
+  lifecycle {
+    ignore_changes = [server_status] 
+  }
+}
+
+resource cloudflare_record frontend_1 {
+  zone_id = file(".secrets/cloudflare_zone_id")
+  type = "A"
+  name = "sandbox-frontend-1"
+  value = vultr_instance.frontend.internal_ip
+  proxied = false
+  allow_overwrite = true
+  comment = "sandbox frontend instance internal"
+  ttl = 300
 }
