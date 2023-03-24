7bcc76444d4bb6a353416f45979d8aff64723f68
Nikita Volobuev <nikitavbv@gmail.com>
2023-03-25 00:18:16 +0200

    feat(utils): do not print sqlx logs

--- server/src/utils.rs
+++ server/src/utils.rs
@@ -1,5 +1,21 @@
+use {
+    tracing::Level,
+    tracing_subscriber::{
+        prelude::*,
+        filter::filter_fn,
+    },
+};
+
 pub fn init_logging() {
     tracing_subscriber::FmtSubscriber::builder()
         .with_max_level(tracing::Level::INFO)
+        .finish()
+        .with(filter_fn(|metadata| {
+            if metadata.target().starts_with("sqlx::query") {
+                metadata.level() > &Level::INFO
+            } else {
+                true
+            }
+        }))
         .init();
 }
