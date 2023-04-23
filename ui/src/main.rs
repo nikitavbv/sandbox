54e12722c053030796523ad874d0e99c2f8b6cab
Nikita Volobuev <nikitavbv@gmail.com>
2023-04-23 23:00:46 +0300

    feat(ui): remove all models other than image generation

--- ui/src/main.rs
+++ ui/src/main.rs
@@ -24,14 +24,6 @@ pub mod components;
 enum Route {
     #[at("/")]
     Home,
-    #[at("/models/image-classification")]
-    ImageClassificationModel,
-    #[at("/models/image-generation")]
-    ImageGenerationModel,
-    #[at("/models/text-generation")]
-    TextGenerationModel,
-    #[at("/model/text-summarization")]
-    TextSummarizationModel,
 }
 
 #[derive(Clone)]
@@ -116,61 +108,11 @@ fn app() -> Html {
 fn router_switch(route: Route) -> Html {
     match route {
         Route::Home => html!(<Home />),
-        Route::ImageClassificationModel => html!(<ImageClassificationModel />),
-        Route::ImageGenerationModel => html!(<ImageGenerationModel />),
-        Route::TextGenerationModel => html!(<TextGenerationModel />),
-        Route::TextSummarizationModel => html!(<TextSummarizationModel />),
     }
 }
 
 #[function_component(Home)]
 fn home() -> Html {
-    let navigator = Arc::new(use_navigator().unwrap());
-
-    let image_generation_btn_handler = {
-        let navigator = navigator.clone();
-
-        Callback::from(move |_| navigator.push(&Route::ImageGenerationModel))
-    };
-
-    let text_generation_btn_handler = {
-        let navigator = navigator.clone();
-
-        Callback::from(move |_| navigator.push(&Route::TextGenerationModel))
-    };
-
-    let text_summarization_btn_handler = Callback::from(move |_| navigator.push(&Route::TextSummarizationModel));
-
-    html!(
-        <div>
-            {"Home"}
-            <button onclick={image_generation_btn_handler}>{"image generation model"}</button>
-            <button onclick={text_generation_btn_handler}>{"text generation model"}</button>
-            <button onclick={text_summarization_btn_handler}>{"text summarization model"}</button>
-        </div>
-    )
-}
-
-#[function_component(ImageClassificationModel)]
-fn image_classification_model() -> Html {
-    let on_file_change = {
-
-        Callback::from(move |_e: InputEvent| {
-            info!("callback is called");
-
-            // TODO: implement reading file here
-        })
-    };
-
-    html!(
-        <>
-            <input type="file" oninput={on_file_change} />
-        </>
-    )
-}
-
-#[function_component(ImageGenerationModel)]
-fn run_image_generation_model() -> Html {
     let navigator = use_navigator().unwrap();
     let client = Arc::new(Mutex::new(client()));
     let state = use_reducer(ModelState::default);
@@ -240,168 +182,6 @@ fn run_image_generation_model() -> Html {
             <h1>{"image generation model"}</h1>
             <input onchange={on_prompt_change} value={state.prompt.clone()} placeholder={"prompt"}/>
             <button onclick={run_inference}>{"run model"}</button>
-            { result }
-        </div>
-    )
-}
- 
-#[function_component(TextGenerationModel)]
-fn text_generation_model() -> Html {
-    let navigator = use_navigator().unwrap();
-    let client = Arc::new(Mutex::new(client()));
-    let state = use_reducer(ModelState::default);
-
-    let go_home_btn_handler = Callback::from(move |_| navigator.push(&Route::Home));
-    let on_prompt_change = {
-        let state = state.clone();
-
-        Callback::from(move |e: Event| {
-            let target: Option<EventTarget> = e.target();
-            let input = target.and_then(|t| t.dyn_into::<HtmlInputElement>().ok());
-            if let Some(input) = input {
-                state.dispatch(ModelAction::UpdatePrompt(input.value()));
-            }
-        })
-    };
-
-    let run_inference = {
-        let state = state.clone();
-        let client = client.clone();
-        
-        let prompt = state.prompt.clone();
-
-        Callback::from(move |_| {
-            let client = client.clone();
-            let prompt = prompt.clone();
-
-            {
-                let state = state.clone();
-
-                spawn_local(async move {
-                    let mut client = client.lock().unwrap();
-                    let res = client.run_text_generation_model(InferenceRequest {
-                        entries: vec![DataEntry {
-                            key: "prompt".to_owned(),
-                            value: Some(data_entry::Value::Text(prompt.clone())),
-                        }],
-                        model: "text-generation".to_owned(),
-                    }).await.unwrap().into_inner();
-    
-                    let text = match res.entries.get(0).unwrap().value.as_ref().unwrap() {
-                        data_entry::Value::Text(text) => text.to_owned(),
-                        other => panic!("unexpected repsonse type"),
-                    };
-
-                    state.dispatch(ModelAction::SetInferenceResult(InferenceResult {
-                        data: InferenceResultData::Text(text),
-                        worker: res.worker,
-                    }));
-                });
-            }
-
-            state.dispatch(ModelAction::StartInference);
-        })
-    };
-
-    let model_controls = if state.inference_started {
-        html!({"running inference..."})
-    } else {
-        html!(<button onclick={run_inference}>{"run model"}</button>)
-    };
-
-    let result = if let Some(result) = &state.result {
-        html!(<InferenceResultDisplay result={result.clone()} />)
-    } else {
-        html!(<div></div>)
-    };
-
-    html!(
-        <div>
-            <button onclick={go_home_btn_handler}>{"home"}</button>
-            <h1>{"text generation model"}</h1>
-            <input onchange={on_prompt_change} value={state.prompt.clone()} placeholder={"prompt"}/>
-            { model_controls }
-            { result }
-        </div>
-    )
-}
-
-#[function_component(TextSummarizationModel)]
-fn text_summarization_model() -> Html {
-    let navigator = use_navigator().unwrap();
-    let client = Arc::new(Mutex::new(client()));
-    let state = use_reducer(ModelState::default);
-
-    let go_home_btn_handler = Callback::from(move |_| navigator.push(&Route::Home));
-    let on_prompt_change = {
-        let state = state.clone();
-
-        Callback::from(move |e: Event| {
-            let target: Option<EventTarget> = e.target();
-            let input = target.and_then(|t| t.dyn_into::<HtmlInputElement>().ok());
-            if let Some(input) = input {
-                state.dispatch(ModelAction::UpdatePrompt(input.value()));
-            }
-        })
-    };
-
-    let run_inference = {
-        let state = state.clone();
-        let client = client.clone();
-
-        let prompt = state.prompt.clone();
-
-        Callback::from(move |_| {
-            let client = client.clone();
-            let prompt = prompt.clone();
-
-            {
-                let state = state.clone();
-
-                spawn_local(async move {
-                    let mut client = client.lock().unwrap();
-                    let res = client.run_model(InferenceRequest {
-                        entries: vec![DataEntry {
-                            key: "text".to_owned(),
-                            value: Some(data_entry::Value::Text(prompt.clone())),
-                        }],
-                        model: "text-summarization".to_owned(),
-                    }).await.unwrap().into_inner();
-
-                    let text = match res.entries.get(0).unwrap().value.as_ref().unwrap() {
-                        data_entry::Value::Text(text) => text.to_owned(),
-                        other => panic!("unexpected response type"),
-                    };
-    
-                    state.dispatch(ModelAction::SetInferenceResult(InferenceResult {
-                        data: InferenceResultData::Text(text),
-                        worker: res.worker,
-                    }));
-                });
-            }
-
-            state.dispatch(ModelAction::StartInference);
-        })
-    };
-
-    let model_controls = if state.inference_started {
-        html!({"running inference..."})
-    } else {
-        html!(<button onclick={run_inference}>{"run model"}</button>)
-    };
-
-    let result = if let Some(result) = &state.result {
-        html!(<InferenceResultDisplay result={result.clone()} />)
-    } else {
-        html!(<div></div>)
-    };
-
-    html!(
-        <div>
-            <button onclick={go_home_btn_handler}>{"home"}</button>
-            <h1>{"text generation model"}</h1>
-            <textarea onchange={on_prompt_change} value={state.prompt.clone()} placeholder={"text to summarize"}></textarea>
-            { model_controls }
             { result }
         </div>
     )
