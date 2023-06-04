31dfb617fb996dd67ec833ad4362ffbddfe57690
Nikita Volobuev <nikitavbv@gmail.com>
2023-06-04 15:36:11 +0300

    feat(worker): add rpc client

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ this is a sandbox to play with various ml models for learning and fun.
 
 # todos
 
-- implement API for workers (finish `submit_task_result` implementation in server).
+- integrate RPC client into worker
--- sandbox-worker/src/main.rs
+++ sandbox-worker/src/main.rs
@@ -1,35 +1,30 @@
 use {
     std::time::Duration,
     tracing::{info, warn},
-    rdkafka::{
-        Message,
-        config::ClientConfig,
-        client::ClientContext,
-        consumer::{StreamConsumer, ConsumerContext, Consumer, CommitMode},
-    },
     tokio::time::sleep,
+    tonic::{
+        codegen::InterceptedService,
+        service::Interceptor,
+        transport::ClientTlsConfig,
+        Status,
+    },
     sandbox_common::{
         utils::{init_logging, load_config},
         messages::TaskMessage,
     },
+    rpc::{
+        ml_sandbox_service_client::MlSandboxServiceClient,
+        GetTaskToRunRequest,
+        SubmitTaskResultRequest,
+    },
     crate::{
         model::StableDiffusionImageGenerationModel,
         storage::Storage,
-        database::Database,
     },
 };
 
-pub mod database;
 pub mod model;
 pub mod storage;
-
-struct StreamingContext;
-
-impl ClientContext for StreamingContext {
-}
-
-impl ConsumerContext for StreamingContext {
-}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -37,72 +32,50 @@ async fn main() -> anyhow::Result<()> {
     let config = load_config();
     
     info!("sandbox worker started");
-    let storage = Storage::new(&config);
-    let database_node = config.get_string("database.node").unwrap();
-    let database = Database::new(&database_node).await.unwrap();
+    let mut client = MlSandboxServiceClient::with_interceptor(
+        tonic::transport::Channel::from_static("https://sandbox.nikitavbv.com/api")
+            .tls_config(ClientTlsConfig::new())
+            .unwrap()
+            .connect()
+            .await
+            .unwrap(),
+        AuthTokenSetterInterceptor::new()
+    );
+    //let storage = Storage::new(&config);
 
-    info!("loading model");
+    /*info!("loading model");
     let model = StableDiffusionImageGenerationModel::new(&storage).await;
     info!("model loaded");
 
-    let consumer: StreamConsumer<StreamingContext> = ClientConfig::new()
-        .set("group.id", "sandbox-worker")
-        .set("bootstrap.servers", &config.get_string("queue.node").unwrap())
-        .set("enable.auto.commit", "true")
-        .create_with_context(StreamingContext)
-        .unwrap();
 
-    consumer.subscribe(&["sandbox-tasks"]).unwrap();
+    info!("generating image for prompt: {}", payload.prompt);
+    let image = model.run(&payload.prompt);
+    info!("finished generating image");
+    storage.save_generated_image(&payload.id, &image).await;
+    database.mark_task_as_complete(&payload.id).await.unwrap();
 
-    loop {
-        match consumer.recv().await {
-            Err(e) => {
-                warn!("kafka error: {:?}", e);
-                sleep(Duration::from_secs(1)).await;
-            },
-            Ok(v) => {
-                let payload = match v.payload_view::<[u8]>() {
-                    None => {
-                        warn!("no payload");
-                        if let Err(err) = consumer.commit_message(&v, CommitMode::Async) {
-                            warn!("failed to commit offsets: {:?}", err);
-                        }
-                        sleep(Duration::from_secs(1)).await;
-                        continue;
-                    },
-                    Some(Ok(v)) => v,
-                    Some(Err(e)) => {
-                        warn!("error while reading payload: {:?}", e);
-                        if let Err(err) = consumer.commit_message(&v, CommitMode::Async) {
-                            warn!("failed to commit offsets: {:?}", err);
-                        }
-                        sleep(Duration::from_secs(1)).await;
-                        continue;
-                    }
-                };
+    if let Err(err) = consumer.commit_message(&v, CommitMode::Async) {
+        warn!("failed to commit offsets: {:?}", err);
+    }*/
 
-                let payload: TaskMessage = match serde_json::from_slice(&payload) {
-                    Ok(v) => v,
-                    Err(err) => {
-                        warn!("error while deserializing payload: {:?}", err);
-                        if let Err(err) = consumer.commit_message(&v, CommitMode::Async) {
-                            warn!("failed to commit offsets: {:?}", err);
-                        }
-                        sleep(Duration::from_secs(1)).await;
-                        continue;
-                    }
-                };
+    Ok(())
+}
 
-                info!("generating image for prompt: {}", payload.prompt);
-                let image = model.run(&payload.prompt);
-                info!("finished generating image");
-                storage.save_generated_image(&payload.id, &image).await;
-                database.mark_task_as_complete(&payload.id).await.unwrap();
+pub struct AuthTokenSetterInterceptor {
+}
 
-                if let Err(err) = consumer.commit_message(&v, CommitMode::Async) {
-                    warn!("failed to commit offsets: {:?}", err);
-                }
-            }
+impl AuthTokenSetterInterceptor {
+    pub fn new() -> Self {
+        Self {
         }
     }
 }
+
+impl Interceptor for AuthTokenSetterInterceptor {
+    fn call(&mut self, mut req: Request<()>) -> Result<Request<()>, Status> {
+        let auth_header_value = MetadataValue::try_from(format!("Bearer token")).expect("failed to create metadata");
+        req.metadata_mut().insert("authorization", auth_header_value);
+        Ok(req)
+    }
+}
+
