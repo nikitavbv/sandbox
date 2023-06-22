94e79a88c47ee46a98b7e05e2fda22a989a9eb1d
Nikita Volobuev <nikitavbv@gmail.com>
2023-06-22 23:50:20 +0300

    feat(infrastructure): remove envoy

--- infrastructure/envoy.sh
+++
@@ -1,7 +0,0 @@
-docker run --rm -it \
-      -p 9901:9901 \
-      -p 443:10000 \
-      -v $(pwd)/config.yaml:/envoy-custom.yaml \
-      envoyproxy/envoy-dev:932e9e36c5d2416c2f0d768bb5ad4db3284417c6 \
-      -c /envoy-custom.yaml
-
