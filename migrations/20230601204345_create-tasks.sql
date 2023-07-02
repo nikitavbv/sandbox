35a1fcb788b61b1531bb00124df08297f5786878
Nikita Volobuev <nikitavbv@gmail.com>
2023-07-03 01:01:24 +0300

    feat(migrations): update tasks table structure

--- migrations/20230601204345_create-tasks.sql
+++ migrations/20230601204345_create-tasks.sql
@@ -1,15 +1,14 @@
--- Add migration script here
 create table sandbox_tasks
 (
     task_id    text                                   not null
         constraint sandbox_tasks_pk
             primary key,
-    status     text                                   not null,
     created_at timestamp with time zone default now() not null,
     prompt     text                                   not null,
-    user_id    text
+    user_id    text,
+    status     jsonb                                  not null,
+    is_pending boolean                                not null
 );
 
 create unique index sandbox_tasks_task_id_uindex
     on sandbox_tasks (task_id);
-
