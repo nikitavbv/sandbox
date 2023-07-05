05bdea450ea48aa64da571fe4512d7d64aef1ed8
Nikita Volobuev <nikitavbv@gmail.com>
2023-07-06 00:06:12 +0300

    feat(ui): auto refresh task status

--- ui/src/pages/task.rs
+++ ui/src/pages/task.rs
@@ -24,13 +24,12 @@ pub fn task_page(props: &TaskPageProps) -> Html {
 
     {
         let client = client.clone();
-        let state = state.clone();
+        let state_setter = state_setter.clone();
 
         use_effect_with_deps(move |id| {
             let client = client.clone();
             let id = id.clone();
             let state_setter = state_setter.clone();
-            let interval = Arc::new(Mutex::new(None::<Interval>));
 
             {
                 let id = id.clone();
@@ -50,86 +49,52 @@ pub fn task_page(props: &TaskPageProps) -> Html {
                 });
             }
             
-            /*let new_interval = {
-                let interval = interval.clone();
-                let refresh_in_progress = Arc::new(AtomicBool::new(false));
-                let id = Some(TaskId { id });
-
-                Interval::new(100, move || {
-                    let state = match &*state {
-                        Some(v) => v,
-                        None => {
-                            info!("no informating about the task has been received yet");
-                            return;
-                        }
-                    };
-
-                    // TODO: implement status refresh
-                    if refresh_in_progress.compare_exchange(false, true, Ordering::SeqCst, Ordering::SeqCst).is_ok() {
-                        info!("sending request to server");
-                    
-                        let client = client.clone();
-                        let id = id.clone();
-                        let refresh_in_progress = refresh_in_progress.clone();
-                        let state_setter = state_setter.clone();
-
-                        spawn_local(async move {
-                            let mut client = client.lock().unwrap();
-
-                            let res = client.get_task(GetTaskRequest {
-                                id,
-                            }).await.unwrap().into_inner();
-
-                            state_setter.set(res.task);
-                        
-                            refresh_in_progress.store(false, Ordering::SeqCst);
-                        });
-                    }
-                    
-                    if let rpc::task::Status::FinishedDetails(_) = state.status.as_ref().unwrap() {
-                        info!("task is ready, there is no reason to refresh it all the time");
-                        interval.lock().unwrap().take().unwrap().cancel();
-                        return;
-                    }
-
-                    info!("waiting for task");
-                })
-            };
-            *interval.lock().unwrap() = Some(new_interval);
-
-            move || {
-                let mut interval = interval.lock().unwrap();
-                
-                if interval.is_some() {
-                    interval.take().unwrap().cancel();
-                }
-            }*/
-
             || {}
         }, props.task_id.clone());
     }
 
-    use_effect_with_deps(|(task_id, status)| {
+    use_effect_with_deps(move |(task_id, status)| {
+        let mut interval = None;            
+        
         if status.is_none() {
             info!("no info about task yet");
         } else if let Some(rpc::task::Status::FinishedDetails(_)) = status {
             info!("task is finished");
         } else {
             info!("task is not finished yet");
-
+            
+            let refresh_in_progress = Arc::new(AtomicBool::new(false));
             let id = task_id.clone();
-            spawn_local(async move {
-                let mut client = client.lock().unwrap();
+            let state_setter = state_setter.clone();
 
-                let res = client.get_task(GetTaskRequest {
-                    id: Some(TaskId { id }),
-                }).await.unwrap();
+            interval = Some(Interval::new(1000, move || {
+                let id = id.clone();
+                let client = client.clone();
 
-                info!("res: {:?}", res);
-            });
+                if refresh_in_progress.compare_exchange(false, true, Ordering::SeqCst, Ordering::SeqCst).is_ok() {
+                    let refresh_in_progress = refresh_in_progress.clone();
+                    let state_setter = state_setter.clone();
+
+                    spawn_local(async move {
+                        let mut client = client.lock().unwrap();
+
+                        let res = client.get_task(GetTaskRequest {
+                            id: Some(TaskId { id }),
+                        }).await.unwrap().into_inner();
+                        
+                        state_setter.set(res.task);
+
+                        refresh_in_progress.store(false, Ordering::SeqCst);
+                    });
+                }
+            }));
         }
 
-        || {}
+        move || {
+            if let Some(interval) = interval {
+                interval.cancel();
+            }
+        }
     }, (props.task_id.clone(), state.as_ref().and_then(|v| v.status.clone())));
 
     let return_home = Callback::from(move |_| {
@@ -157,11 +122,4 @@ pub fn task_page(props: &TaskPageProps) -> Html {
             { rendered }
         </div>
     )
-}
-
-fn is_finished_task(task: &Task) -> bool {
-    match task.status.as_ref().unwrap() {
-        rpc::task::Status::FinishedDetails(_) => true,
-        _ => false,
-    }
 }
