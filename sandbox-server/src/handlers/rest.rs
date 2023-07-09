78eb90a43bfd6476668fc49737be4d35db39c990
Nikita Volobuev <nikitavbv@gmail.com>
2023-07-09 16:04:51 +0300

    feat(server): add oauth callback endpoint

--- sandbox-server/src/handlers/rest.rs
+++ sandbox-server/src/handlers/rest.rs
@@ -1,7 +1,15 @@
 use {
     std::sync::Arc,
     serde::Deserialize,
-    axum::{Router, Extension, response::Response, extract::Path, routing::get, http::header::{CONTENT_TYPE, HeaderValue}, body::Body},
+    axum::{
+        Router, 
+        Extension,
+        response::{Response, IntoResponse}, 
+        extract::Path, 
+        routing::get, 
+        http::header::{CONTENT_TYPE, HeaderValue}, 
+        body::Body,
+    },
     crate::{
         entities::TaskId,
         state::database::Database,
@@ -16,6 +24,7 @@ pub struct AssetID {
 pub fn rest_router(database: Arc<Database>) -> Router {
     Router::new()
         .route("/v1/storage/:asset_id", get(serve_asset))
+        .route("/v1/auth/callback", get(auth_callback))
         .layer(Extension(database))
 }
 
@@ -25,4 +34,23 @@ async fn serve_asset(Extension(database): Extension<Arc<Database>>, Path(asset_i
     let mut res = Response::new(Body::from(body));
     res.headers_mut().insert(CONTENT_TYPE, HeaderValue::from_static("image/png"));
     res
+}
+
+async fn auth_callback() -> impl IntoResponse {
+    let client = reqwest::Client::new();
+
+    // TODO: finish this
+    /*let res = client
+        .post("https://oauth2.googleapis.com/token")
+        .form(&[
+            ("client_id", client_id),
+            ("client_secret", client_secret),
+            ("code", code),
+            ("grant_type", "authorization_code"),
+            ("redirect_uri", redirect_uri()),
+        ])
+        .send()
+        .await;*/
+
+    unimplemented!()
 }
