5c72809d5058ffb1fcb52527504b59c62440e60b
Nikita Volobuev <nikitavbv@gmail.com>
2023-08-20 23:17:09 +0300

    feat(server): remove pytorch env scripts

--- linux-gpu-env.sh
+++
@@ -1,8 +0,0 @@
-if [ ! -d "./libtorch" ]
-then
-    wget https://download.pytorch.org/libtorch/cu117/libtorch-cxx11-abi-shared-with-deps-2.0.0%2Bcu117.zip
-    unzip libtorch-cxx11-abi-shared-with-deps-2.0.0+cu117.zip
-fi
-
-export LIBTORCH=$(pwd)/libtorch
-export LD_LIBRARY_PATH=${LIBTORCH}/lib:$LD_LIBRARY_PATH
--- macos-env.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export LIBTORCH=$(python3 -c 'import torch; from pathlib import Path; print(Path(torch.__file__).parent)')
-export DYLD_LIBRARY_PATH=${LIBTORCH}/lib
