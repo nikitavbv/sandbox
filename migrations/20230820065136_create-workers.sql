4341ffaf036e32b2a6b0e1f3e7e046c598a90b84
Nikita Volobuev <nikitavbv@gmail.com>
2023-08-20 10:02:49 +0300

    fix(migrations): fix syntax error

--- migrations/20230820065136_create-workers.sql
+++ migrations/20230820065136_create-workers.sql
@@ -1,6 +1,6 @@
 create table sandbox_workers
 (
-    last_ping_at timestamp with time zone default now() not null,
+    last_ping_at timestamp with time zone default now() not null
 );
 
 insert into sandbox_workers (last_ping_at) values (now());
